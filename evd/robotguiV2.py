4f8ba48cd227336e08ece53c1b971412e786bda1
Hunter Sullivan <hcsullivan12@yahoo.com>
2019-09-28 14:42:19 -0500

    Added functionality to step to predefined positions from text file. Removed unused buttons.

--- evd/robotguiV2.py
+++ evd/robotguiV2.py
@@ -31,7 +31,6 @@ x1,x2,x3,x4,myUI=0,0,0,0,None
 # option for input steering file
 doSteering = True
 steeringSteps = list()
-stepping = False
 
 # Stepper motor and disk information
 disk_r   = 0.5*100 #cm
@@ -48,14 +47,18 @@ emulator = emu.Emulator()
 
 class Ui_Dialog(QtCore.QObject):
     updatePosition = pyqtSignal()
+    moveIt = pyqtSignal()
     stepperThread = None 
     currentStep = 0
+    stepping = False
 
     def __init__(self):
         QtCore.QObject.__init__(self)
         self.myCanvas = None
         self.position = {'x':0.0, 'y':0.0}
-        self.updatePosition.connect(self.Update)
+        self.target = None
+        self.updatePosition.connect(self.update)
+        self.moveIt.connect(self.doMove)
 
     def getCanvas(self):
         return self.myCanvas
@@ -63,41 +66,63 @@ class Ui_Dialog(QtCore.QObject):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(903, 506)
-        self.clockwiset = QtGui.QPushButton(Dialog)
-        self.clockwiset.setGeometry(QtCore.QRect(400, 60, 120, 50))
-        self.clockwiset.setObjectName("clockwiset")
-        self.clockwiseb = QtGui.QPushButton(Dialog)
-        self.clockwiseb.setGeometry(QtCore.QRect(400, 110, 120, 50))
-        self.clockwiseb.setObjectName("clockwiseb")
-        self.counterclockwiseb = QtGui.QPushButton(Dialog)
-        self.counterclockwiseb.setGeometry(QtCore.QRect(550, 110, 120, 50))
-        self.counterclockwiseb.setObjectName("counterclockwiseb")
-        self.counterclockwiset = QtGui.QPushButton(Dialog)
-        self.counterclockwiset.setGeometry(QtCore.QRect(550, 60, 120, 50))
-        self.counterclockwiset.setObjectName("counterclockwiset")
-        self.forwardb = QtGui.QPushButton(Dialog)
-        self.forwardb.setGeometry(QtCore.QRect(400, 270, 120, 50))
-        self.forwardb.setObjectName("forwardb")
-        self.backwardb = QtGui.QPushButton(Dialog)
-        self.backwardb.setGeometry(QtCore.QRect(550, 270, 120, 50))
-        self.backwardb.setObjectName("backwardb")
-        self.forwardt = QtGui.QPushButton(Dialog)
-        self.forwardt.setGeometry(QtCore.QRect(400, 220, 120, 50))
-        self.forwardt.setObjectName("forwardt")
-        self.backwardt = QtGui.QPushButton(Dialog)
-        self.backwardt.setGeometry(QtCore.QRect(550, 220, 120, 50))
-        self.backwardt.setObjectName("backwardt")
+
+        self.makeManualSteer()
+        self.makeLabels()
+        self.makeManualInput()
+        
+        self.figure = plt.figure() # @hunter figsize=(350,350)
+        self.myCanvas = PlotCanvas(Dialog, width=3.5, height=3.5)
+        self.myCanvas.move(40,60)
+        
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+        self.connectThem()
+
+    def makeManualSteer(self):
+        self.butClockwise = QtGui.QPushButton(Dialog)
+        self.butClockwise.setGeometry(QtCore.QRect(400, 50, 130, 50))
+        self.butClockwise.setObjectName("butClockwise")
+        self.butCounterbutClockwise = QtGui.QPushButton(Dialog)
+        self.butCounterbutClockwise.setGeometry(QtCore.QRect(540, 50, 130, 50))
+        self.butCounterbutClockwise.setObjectName("butCounterbutClockwise")
+        self.butForward = QtGui.QPushButton(Dialog)
+        self.butForward.setGeometry(QtCore.QRect(400, 140, 130, 50))
+        self.butForward.setObjectName("butForward")
+        self.butBackward = QtGui.QPushButton(Dialog)
+        self.butBackward.setGeometry(QtCore.QRect(540, 140, 130, 50))
+        self.butBackward.setObjectName("butBackward")
+        self.butSetPosition = QtGui.QPushButton(Dialog)
+        self.butSetPosition.setGeometry(QtCore.QRect(540, 220, 130, 50))
+        self.butSetPosition.setObjectName("butSetPosition")
+        self.butZero = QtGui.QPushButton(Dialog)
+        self.butZero.setGeometry(QtCore.QRect(155, 420, 120, 50))
+        font = QtGui.QFont()
+        font.setPointSize(8)
+        self.butZero.setFont(font)
+        self.butZero.setObjectName("butZero")
+        self.butQuit = QtGui.QPushButton(Dialog)
+        self.butQuit.setGeometry(QtCore.QRect(450, 420, 120, 50))
+        self.butQuit.setObjectName("butQuit")
+
         self.stepsForRotation = QtGui.QPlainTextEdit(Dialog)
-        self.stepsForRotation.setGeometry(QtCore.QRect(700, 60, 120, 50))
+        self.stepsForRotation.setGeometry(QtCore.QRect(700, 50, 120, 50))
         self.stepsForRotation.setObjectName("stepsForRotation")
         self.stepsForLinear = QtGui.QPlainTextEdit(Dialog)
-        self.stepsForLinear.setGeometry(QtCore.QRect(700, 220, 120, 50))
+        self.stepsForLinear.setGeometry(QtCore.QRect(700, 140, 120, 50))
         self.stepsForLinear.setObjectName("stepsForLinear")
-        #self.inputPosition = QtGui.QPlainTextEdit(Dialog)
-        #self.inputPosition.setGeometry(QtCore.QRect(700, 325, 120, 50))
-        #self.inputPosition.setObjectName("stepsForLinear")
+
+        self.positionInputX = QtGui.QPlainTextEdit(Dialog)
+        self.positionInputX.setGeometry(QtCore.QRect(420, 220, 110, 25))
+        self.positionInputX.setObjectName("positionInputX")
+        self.positionInputY = QtGui.QPlainTextEdit(Dialog)
+        self.positionInputY.setGeometry(QtCore.QRect(420, 250, 110, 25))
+        self.positionInputY.setObjectName("positionInputY")
+
+    def makeLabels(self):
         self.label = QtGui.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(475, 20, 121, 31))
+        self.label.setGeometry(QtCore.QRect(480, 20, 121, 31))
         font = QtGui.QFont()
         font.setPointSize(20)
         font.setBold(False)
@@ -105,7 +130,7 @@ class Ui_Dialog(QtCore.QObject):
         self.label.setFont(font)
         self.label.setObjectName("label")
         self.label_2 = QtGui.QLabel(Dialog)
-        self.label_2.setGeometry(QtCore.QRect(490, 180, 91, 31))
+        self.label_2.setGeometry(QtCore.QRect(495, 110, 91, 31))
         font = QtGui.QFont()
         font.setPointSize(20)
         font.setBold(False)
@@ -121,141 +146,122 @@ class Ui_Dialog(QtCore.QObject):
         self.label_3.setFont(font)
         self.label_3.setObjectName("label_3")
         self.label_4 = QtGui.QLabel(Dialog)
-        self.label_4.setGeometry(QtCore.QRect(720, 180, 81, 31))
+        self.label_4.setGeometry(QtCore.QRect(720, 110, 81, 31))
         font = QtGui.QFont()
         font.setPointSize(20)
         font.setBold(False)
         font.setWeight(50)
         self.label_4.setFont(font)
         self.label_4.setObjectName("label_4")
-        self.zero = QtGui.QPushButton(Dialog)
-        self.zero.setGeometry(QtCore.QRect(155, 420, 120, 50))
-        font = QtGui.QFont()
-        font.setPointSize(8)
-        self.zero.setFont(font)
-        self.zero.setObjectName("zero")
-        self.quit = QtGui.QPushButton(Dialog)
-        self.quit.setGeometry(QtCore.QRect(450, 420, 120, 50))
-        self.quit.setObjectName("quit")
         self.label_5 = QtGui.QLabel(Dialog)
-        self.label_5.setGeometry(QtCore.QRect(485, 330, 100, 31))
-        font = QtGui.QFont()
-        font.setPointSize(20)
-        font.setBold(False)
-        font.setWeight(50)
-        self.label_5.setFont(font)
-        self.label_5.setObjectName("label_5")
+        #self.label_5.setGeometry(QtCore.QRect(485, 330, 100, 31))
+        #font = QtGui.QFont()
+        #font.setPointSize(20)
+        #font.setBold(False)
+        #font.setWeight(50)
+        #self.label_5.setFont(font)
+        #self.label_5.setObjectName("label_5")
         self.label_6 = QtGui.QLabel(Dialog)
-        self.label_6.setGeometry(QtCore.QRect(395, 370, 10, 20))
+        self.label_6.setGeometry(QtCore.QRect(400, 220, 20, 20))
         self.label_6.setObjectName("label_6")
         self.label_7 = QtGui.QLabel(Dialog)
-        self.label_7.setGeometry(QtCore.QRect(540, 370, 10, 20))
+        self.label_7.setGeometry(QtCore.QRect(400, 250, 20, 20))
         self.label_7.setObjectName("label_7")
-        self.positionInputX = QtGui.QPlainTextEdit(Dialog)
-        self.positionInputX.setGeometry(QtCore.QRect(410, 370, 120, 25))
-        self.positionInputX.setObjectName("positionInputX")
-        self.positionInputY = QtGui.QPlainTextEdit(Dialog)
-        self.positionInputY.setGeometry(QtCore.QRect(555, 370, 120, 25))
-        self.positionInputY.setObjectName("positionInputY")
-        self.setPosition = QtGui.QPushButton(Dialog)
-        self.setPosition.setGeometry(QtCore.QRect(700, 360, 120, 50))
-        self.setPosition.setObjectName("setPosition")
         self.label_8 = QtGui.QLabel(Dialog)
         self.label_8.setGeometry(QtCore.QRect(650, 450, 120, 20))
         self.label_8.setObjectName("label_8")
         self.currentPositionLabel = QtGui.QLabel(Dialog)
         self.currentPositionLabel.setGeometry(QtCore.QRect(770, 450, 120, 20))
         self.currentPositionLabel.setObjectName("currentPositionLabel")
-        self.runStepper = QtGui.QPushButton(Dialog)
-        self.runStepper.setGeometry(QtCore.QRect(700, 300, 120, 50))
-        self.runStepper.setObjectName("runStepper")
-        
-        self.figure = plt.figure() # @hunter figsize=(350,350)
-        self.myCanvas = PlotCanvas(Dialog, width=3.5, height=3.5)
-        self.myCanvas.move(40,60)
-        
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
 
-        # connecting buttons
-        self.clockwiseb.clicked.connect(self.Clockwiseb)
-        self.counterclockwiseb.clicked.connect(self.Counterclockwiseb)
-        self.clockwiset.clicked.connect(self.Clockwiset)
-        self.counterclockwiset.clicked.connect(self.Counterclockwiset)
-        self.forwardb.clicked.connect(self.Forwardb)
-        self.forwardt.clicked.connect(self.Forwardt)
-        self.zero.clicked.connect(self.Zero)
-        self.backwardb.clicked.connect(self.Backwardb)
-        self.backwardt.clicked.connect(self.Backwardt)
-        self.quit.clicked.connect(self.Quit)
-        self.setPosition.clicked.connect(self.SetPosition)
-        self.runStepper.clicked.connect(self.StartStepper)
+    def makeManualInput(self):
+        self.butRunStepper = QtGui.QPushButton(Dialog)
+        self.butRunStepper.setGeometry(QtCore.QRect(400, 300, 130, 50))
+        self.butRunStepper.setObjectName("butRunStepper")
+        self.butResetStepper = QtGui.QPushButton(Dialog)
+        self.butResetStepper.setGeometry(QtCore.QRect(540, 300, 130, 50))
+        self.butResetStepper.setObjectName("butResetStepper")
+
+    def connectThem(self):
+        self.butClockwise.clicked.connect(self.doClockwise)
+        self.butCounterbutClockwise.clicked.connect(self.doCounterClockwise)
+        self.butForward.clicked.connect(self.doForward)
+        self.butZero.clicked.connect(self.doZero)
+        self.butBackward.clicked.connect(self.doBackward)
+        self.butQuit.clicked.connect(self.doQuit)
+        self.butSetPosition.clicked.connect(self.doSetPosition)
+        self.butRunStepper.clicked.connect(self.doStartStepper)
+        self.butResetStepper.clicked.connect(self.doResetStepper)
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.clockwiset.setText(_translate("Dialog", "Top \n" "Clockwise"))
-        self.clockwiseb.setText(_translate("Dialog", "Bottom \n" "Clockwise"))
-        self.counterclockwiseb.setText(_translate("Dialog", "Bottom \n" "CounterClockwise"))
-        self.counterclockwiset.setText(_translate("Dialog", "Top \n" "CounterClockwise"))
-        self.forwardb.setText(_translate("Dialog", "Bottom \n" "Forward"))
-        self.backwardb.setText(_translate("Dialog", "Bottom \n" "Backward"))
-        self.forwardt.setText(_translate("Dialog", "Top \n" "Forward"))
-        self.backwardt.setText(_translate("Dialog", "Top \n" "Backward"))
+        self.butClockwise.setText(_translate("Dialog", "Clockwise"))
+        self.butCounterbutClockwise.setText(_translate("Dialog", "CounterClockwise"))
+        self.butForward.setText(_translate("Dialog", "Forward"))
+        self.butBackward.setText(_translate("Dialog", "Backward"))
         self.label.setText(_translate("Dialog", "Rotation"))
         self.label_2.setText(_translate("Dialog", "Linear"))
         self.label_3.setText(_translate("Dialog", "Steps"))
         self.label_4.setText(_translate("Dialog", "Steps"))
-        self.label_5.setText(_translate("Dialog", "Position"))
-        self.label_6.setText(_translate("Dialog", "X: "))
-        self.label_7.setText(_translate("Dialog", "Y: "))
-        self.zero.setText(_translate("Dialog", "Zero"))
+        #self.label_5.setText(_translate("Dialog", "Position"))
+        self.label_6.setText(_translate("Dialog", "X :"))
+        self.label_7.setText(_translate("Dialog", "Y :"))
+        self.butZero.setText(_translate("Dialog", "zero"))
         self.stepsForRotation.setPlainText(_translate("Dialog", "0"))
         self.stepsForLinear.setPlainText('0')
-        self.quit.setText(_translate("Dialog", "Quit"))
+        self.butQuit.setText(_translate("Dialog", "quit"))
         self.positionInputX.setPlainText('0')
         self.positionInputY.setPlainText('0')
-        self.setPosition.setText(_translate("Dialog", "Set position"))
+        self.butSetPosition.setText(_translate("Dialog", "Set position"))
         self.label_8.setText(_translate("Dialog", "Current position: "))
         temp = '('+str(self.position['x'])+','+str(self.position['y'])+')'
         self.currentPositionLabel.setText(_translate("Dialog", temp))
-        self.runStepper.setText(_translate("Dialog", "Start Stepper"))
+        self.butRunStepper.setText(_translate("Dialog", "Start Stepper"))
+        self.butResetStepper.setText(_translate("Dialog", "Reset Stepper"))
         
-    def Update(self):
+    def update(self):
         _translate = QtCore.QCoreApplication.translate
         temp = "({:.2f}, {:.2f})".format(self.position['x'], self.position['y'])
         self.currentPositionLabel.setText(_translate("Dialog", temp))
     
-    def StartStepper(self):
+    def doResetStepper(self):
+        self.currentStep = 0
+    
+    def doStartStepper(self):
         _translate = QtCore.QCoreApplication.translate
-        if not stepping:
-            self.runStepper.setText(_translate("Dialog", "Stop Stepper"))
-            stepping = True
+        if not self.stepping:
+            self.butRunStepper.setText(_translate("Dialog", "Stop Stepper"))
+            self.stepping = True
             self.stepperThread = threading.Thread(target=self.doStep)
             self.stepperThread.daemon = True
             self.stepperThread.start()
         else:
-            self.stepperThread.stop()
-            stepping = False
-            self.runStepper.setText(_translate("Dialog", "Start Stepper"))
+            self.stepping = False
+            self.butRunStepper.setText(_translate("Dialog", "Start Stepper"))
 
     def doStep(self):
-        while currentStep < len(steeringSteps):
-            currentStep += 1
+        while self.stepping and self.currentStep < len(steeringSteps):
+            x,y = steeringSteps[self.currentStep]
+            self.target = {'x':x, 'y':y}
+            self.moveIt.emit()
+            time.sleep(2) # we need time to get there
+            self.currentStep += 1
 
-
-        
-        
-
-    def Quit(self):
+    def doQuit(self):
         QtCore.QCoreApplication.instance().quit()
     
-    def SetPosition(self):
+    def doSetPosition(self):
         '''
         Set the position manually.
         '''
+        x,y = float(self.positionInputX.toPlainText()), float(self.positionInputY.toPlainText())
+        self.target = {'x':x, 'y':y}
+        self.moveIt.emit()
+
+    def doMove(self):
         currentX, currentY = self.position['x'], self.position['y']
-        targetX, targetY = float(self.positionInputX.toPlainText()), float(self.positionInputY.toPlainText())
+        targetX, targetY   = self.target['x'], self.target['y']
 
         # compute the angle
         import numpy as np
@@ -273,59 +279,44 @@ class Ui_Dialog(QtCore.QObject):
         _translate = QtCore.QCoreApplication.translate
         self.stepsForRotation.setPlainText(_translate("Dialog", str(abs(angleSteps))))
         self.stepsForLinear.setPlainText(_translate("Dialog", str(abs(distSteps))))
+        print deltaDist, deltaTheta
         if angleSteps < 0:
-            self.Clockwiseb()
+            self.doCounterClockwise()
         else:
-            self.Counterclockwiseb()
+            self.doClockwise()
         if distSteps < 0:
-            self.Backwardb()
+            self.doBackward()
         else:
-            self.Forwardb()
+            self.doForward()
         self.position = {'x':targetX, 'y':targetY}
-        self.Update()
+        self.update()
     
-    def Clockwiseb(self):
+    def doClockwise(self):
         print("Clockwise")
-        self.SerialWrite('a', self.stepsForRotation, sleep=0.01)
-        
-    def Clockwiset(self):
-        print("Clockwise")
-        self.SerialWrite('c', self.stepsForRotation, sleep=0.01)
+        self.doSerialWrite('a', self.stepsForRotation, sleep=0.01)
                 
-    def Counterclockwiseb(self):
+    def doCounterClockwise(self):
         print("CounterClockwise")
-        self.SerialWrite('b', self.stepsForRotation, sleep=0.1)
+        self.doSerialWrite('b', self.stepsForRotation, sleep=0.1)
         
-    def Counterclockwiset(self):
-        print("CounterClockwise")
-        self.SerialWrite('d', self.stepsForRotation, sleep=0.1)
-        
-    def Backwardb(self):
+    def doBackward(self):
         print("Backward")
-        self.SerialWrite('e', self.stepsForLinear, sleep=0.1)
-        
-    def Backwardt(self):
-        print("Backward")
-        self.SerialWrite('g', self.stepsForLinear, sleep=0.1)
+        self.doSerialWrite('e', self.stepsForLinear, sleep=0.1)
 
-    def Forwardb(self):
+    def doForward(self):
         print("Forward")
-        self.SerialWrite('f', self.stepsForLinear, sleep=0.1)
+        self.doSerialWrite('f', self.stepsForLinear, sleep=0.1)
         
-    def Forwardt(self):
-        print("Forward")
-        self.SerialWrite('h', self.stepsForLinear, sleep=0.1)
-        
-    def Zero(self):
+    def doZero(self):
         global x1, x2, x3, x4
-        print("Zero")
+        print("zero")
         var = 'e'
         ArduinoSerial.write(var.encode())
         time.sleep(5)
         x1,x2,x3,x4=0,0,0,0
         myCanvas.plot(x1, x2, x3, x4)
 
-    def SerialWrite(self, var, textEdit, sleep=0.1):
+    def doSerialWrite(self, var, textEdit, sleep=0.1):
         #ArduinoSerial.write(var.encode()) @hunter
         emulator.setData(var, int(float(textEdit.toPlainText()))) # @hunter in case we get 0.0
         time.sleep(sleep)
@@ -391,7 +382,7 @@ class PlotCanvas(FigureCanvas):
         return xl,yl
         
 if doSteering:
-    with open('evd/steering.txt') as f:
+    with open('steering.txt') as f:
         while True:
             lineVec = f.readline().split()
             if len(lineVec) < 1:
